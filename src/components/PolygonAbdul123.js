c672754cc7d34d1bfb66f8033d365eff191b891b
Abdul Mutaal Azhar <abdulmutaalazhar@gmail.com>
2022-10-31 15:44:23 +0500

    first commit

---
+++ src/components/PolygonAbdul123.js
@@ -0,0 +1,12 @@
+export default function Polygon(props) {
+    const square = {
+      background: props.color,
+      width: "100px",
+      height: "100px"
+    };
+
+   return (
+      <div style={eval(props.shape)}>
+      </div>
+    );
+}
